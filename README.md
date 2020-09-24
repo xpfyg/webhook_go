4b5717f068f28616b780bec141dce68b3dac2ea5
chihuan <chihuan1>
2020-09-24 16:15:19 +0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # webhook_go
+
+### 描述
+    gitlab 的webhook 服务，可自定义shell脚本实现自动发布
+    sbin目录命名规则为 [project_id].sh,project_id指gitlab项目ID
+
+### 使用
+```
+# go build
+# p 为端口 t为X-Gitlab-Token
+# ./webhook_go -p=8080 -t=1234
+```
--- main.go
+++ main.go
@@ -1,42 +1,120 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
+	"os"
+	"os/exec"
+	"strconv"
 )
 
-func main() {
-	r := gin.Default()
-	gin.SetMode(gin.TestMode)
-	r.POST("/webhook", Webhook)
-	r.Run(":8080")
+var (
+	h     bool
+	p     string
+	t     string
+	TOKEN string
+)
+
+func init() {
+	flag.StringVar(&t, "t", "", "X-Gitlab-Token")
+	flag.StringVar(&p, "p", "", "端口")
+	flag.Usage = usage
+}
+func usage() {
+	fmt.Fprintf(os.Stderr, `webhook_go webhook工具, 支持自定义shell脚本
+Usage: webhook_go [-p] [-t]
+
+Options:
+`)
+	flag.PrintDefaults()
 }
 
-type loginReq struct {
-	Message string
+func main() {
+	flag.Parse()
+	if p == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	r.Use(CheckHeader())
+	r.POST("/webhook", Webhook)
+	TOKEN = t
+	err := r.Run(fmt.Sprintf(":%s", p))
+	if err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
+}
+
+type WebhookReq struct {
+	ProjectId         int      `json:"project_id"`
+	Ref               string   `json:"ref"`
+	Commits           []Commit `json:"commits"`
+	TotalCommitsCount int      `json:"total_commits_count"`
+}
+type Commit struct {
+	Id       string   `json:"id"`
+	Message  string   `json:"message"`
+	Added    []string `json:"added"`
+	Modified []string `json:"modified"`
+	Removed  []string `json:"removed"`
 }
 
 func Webhook(c *gin.Context) {
-	headers := c.Request.Header
-	for k, _ := range headers {
-		println(k + "==::==" + headers.Get(k))
-	}
-	body, _ := ioutil.ReadAll(c.Request.Body)
-	println("---body/--- \r\n " + string(body))
-
+	var req WebhookReq
+	c.BindJSON(&req)
+	Process(req)
 }
 
-func CheckParamAndHeader(h func(a *gin.Context)) gin.HandlerFunc {
-	return func(c *gin.Context) {
+func Process(req WebhookReq) {
+	shell_path := "sbin/" + strconv.Itoa(req.ProjectId) + ".sh"
+	println(shell_path)
+	cmd := exec.Command("sh", shell_path)
+	// 命令的错误输出和标准输出都连接到同一个管道
+	stdout, err := cmd.StdoutPipe()
+	cmd.Stderr = cmd.Stdout
+	if err != nil {
+		println("Process 1", err.Error())
+		return
+	}
 
-		header := c.Request.Header.Get("token")
-		if header == "" {
+	if err = cmd.Start(); err != nil {
+		println("Process 2", err.Error())
+		return
+	}
+	// 从管道中实时获取输出并打印到终端
+	println("/---------------**************------------\\")
+	for {
+		tmp := make([]byte, 1024)
+		_, err := stdout.Read(tmp)
+		print(string(tmp))
+		if err != nil {
+			break
+		}
+	}
+	println("\\---------------**************------------/")
+	if err = cmd.Wait(); err != nil {
+		println("Process 2", err.Error())
+		return
+	}
+}
+
+func CheckHeader() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if TOKEN == "" {
+			return
+		}
+		token := c.Request.Header.Get("X-Gitlab-Token")
+		if token == "" {
 			c.JSON(200, gin.H{
 				"code":   3,
 				"result": "failed",
 				"msg":    ". Missing token",
 			})
-			return
+			c.Abort()
 		}
 	}
 }
